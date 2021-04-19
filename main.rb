4fa0c491d85a960a02519be06c93b46e47b952be
Kenny Ortega <kennyaoc@gmail.com>
2021-04-19 18:26:36 -0400

    Completed bubble_sort

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
-puts 'Hello World'
+def bubble_sort(arr)
+    puts "arr= #{arr}"
+    for i in (0..arr.length-2)
+        if arr[i]>arr[i+1]
+            str = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = str
+            arr = bubble_sort(arr)
+        end
+    end
+    arr
+end
+
+arr = [4, 3, 78, 2, 0, 2]
+
+puts "Integer Unordered Array #{arr}"
+puts "Integer Ordered Array   #{bubble_sort(arr)}"
