8d5394f0aa09d3a531902c7052a998ed86751e20
Kenny Ortega <kennyaoc@gmail.com>
2021-04-19 18:29:12 -0400

    Fixed linters

--- main.rb
+++ main.rb
@@ -1,14 +1,14 @@
 def bubble_sort(arr)
-    puts "arr= #{arr}"
-    for i in (0..arr.length-2)
-        if arr[i]>arr[i+1]
-            str = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = str
-            arr = bubble_sort(arr)
-        end
-    end
-    arr
+  puts "arr= #{arr}"
+  for i in (0..arr.length - 2)
+      if arr[i]>arr[i + 1]
+          str = arr[i]
+          arr[i] = arr[i + 1]
+          arr[i + 1] = str
+          arr = bubble_sort(arr)
+      end
+  end
+  arr
 end
 
 arr = [4, 3, 78, 2, 0, 2]
