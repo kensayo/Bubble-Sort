aec161679c42c7cb4896198df062869148353187
Kenny Ortega <kennyaoc@gmail.com>
2021-04-20 12:16:35 -0400

    Fixed linters

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
 def bubble_sort(arr)
   puts "arr = #{arr}"
   (arr.size - 2).times do |i|
-  if arr[i] > arr[i + 1]
-        str, arr[i] = arr[i], arr[i + 1]
-        arr[i + 1] = str
-        arr = bubble_sort(arr)
-    end
+    next if arr[i] > arr[i + 1]
+
+    str = arr[i]
+    arr[i] = arr[i + 1]
+    arr[i + 1] = str
+    arr = bubble_sort(arr)
   end
   arr
 end
@@ -13,4 +14,5 @@ end
 arr = [4, 3, 78, 2, 0, 2]
 
 puts "Integer Unordered Array #{arr}"
+
 puts "Integer Ordered Array   #{bubble_sort(arr)}"
