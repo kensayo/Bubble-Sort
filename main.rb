c2bdcb16d2199b86479a1e0d7e77d4fd06951c8b
Kenny Ortega <kennyaoc@gmail.com>
2021-04-20 14:05:27 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,8 @@
 def bubble_sort(arr)
-  puts "arr = #{arr}"
   (arr.size - 2).times do |i|
     next if arr[i] > arr[i + 1]
 
-    str = arr[i]
-    arr[i] = arr[i + 1]
-    arr[i + 1] = str
+    arr[i], arr[i + 1] = arr[i + 1], arr[i]
     arr = bubble_sort(arr)
   end
   arr
@@ -16,3 +13,23 @@ arr = [4, 3, 78, 2, 0, 2]
 puts "Integer Unordered Array #{arr}"
 
 puts "Integer Ordered Array   #{bubble_sort(arr)}"
+
+def bubble_sort_by(arr)
+  (arr.size - 1).times do |i|
+    if (yield arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      bubble_sort_by(arr) { |left, right| left.length - right.length }
+    end
+  end
+  arr
+end
+
+string_array = %w[hi hello hey]
+
+puts "String Unordered Array  #{string_array}"
+
+bubble_sort_by(string_array) do |left, right|
+  left.length - right.length
+end
+
+puts "String Ordered Array    #{string_array}"
