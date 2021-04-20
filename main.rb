b2ddf972735c355bcdb8515a3a7f80e248392aa0
Kenny Ortega <kennyaoc@gmail.com>
2021-04-19 21:58:57 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
 def bubble_sort(arr)
-  puts "arr= #{arr}"
+  puts "arr = #{arr}"
   for i in (0..arr.length - 2)
-      if arr[i]>arr[i + 1]
-          str = arr[i]
-          arr[i] = arr[i + 1]
+      if arr[i] > arr[i + 1]
+          str, arr[i] = arr[i], arr[i + 1]
           arr[i + 1] = str
           arr = bubble_sort(arr)
       end
