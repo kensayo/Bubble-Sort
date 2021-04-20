8159fc906ee1064bc19d1e02e0541754b11518ae
Kenny Ortega <kennyaoc@gmail.com>
2021-04-20 19:37:39 -0400

    Added some outputs
    Added some outputs so is easier to understand

--- main.rb
+++ main.rb
@@ -1,17 +1,19 @@
 def bubble_sort_by(arr)
-    (arr.size - 1).times do |i|
-      if (yield arr[i], arr[i + 1]).positive?
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        bubble_sort_by(arr) { |left, right| left.length - right.length }
-      end
+  (arr.size - 1).times do |i|
+    if (yield arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      bubble_sort_by(arr) { |left, right| left.length - right.length }
     end
-    arr
   end
-  
-  string_array = %w[hi hello hey]
-  
-  bubble_sort_by(string_array) do |left, right|
-    left.length - right.length
-  end
-  
-  
+  arr
+end
+
+string_array = %w[hi hello hey]
+
+puts "String Unordered Array  #{string_array}"
+
+bubble_sort_by(string_array) do |left, right|
+  left.length - right.length
+end
+
+puts "String Ordered Array    #{string_array}"
